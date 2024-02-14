dfabcf121a280b4f5b7c8a56095bc9931faaf6e6
RyanPivirotto <ryanpivirotto@gmail.com>
2024-02-14 13:47:46 -0700

    lab 5 complete

---
+++ lab5/views.py
@@ -0,0 +1,43 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
+
+class HomePageView(View):
+
+    template_name = 'lab5/index.html'
+
+    def get(self, request):
+
+        form = FeedbackForm()
+     
+        return render(request, self.template_name,{'form':form})
+
+
+    def post(self, request):
+
+        form = FeedbackForm(request.POST)
+
+        my_message = ""
+        service_selected = False
+
+        if form.is_valid():
+            my_message = form.cleaned_data['my_message']
+            service_selected = 'srvc' in form.cleaned_data['review_area']
+
+            return redirect('thanks/')
+        else:    
+            return render(request, self.template_name,{'form':form,'my_message':my_message,'service_selected':service_selected})
+
+class ThankYouPageView(View):
+
+       template_name = 'lab5/thanks.html'
+
+       def get(self, request):
+
+            name = 'Ryan Pivirotto'
+
+            return render(request, self.template_name,{'my_name':name}) 
+        
+
+
+
